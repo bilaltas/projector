db176320b67eda4507beee48f7ebd5f6eb5bda92
Bilal TAS <bilaltas@me.com>
2019-09-02 12:58:03 +0300

    Bug fixes

--- config/wp-cli-install.sh
+++ config/wp-cli-install.sh
@@ -3,3 +3,6 @@
 apt-get update -qq &> /dev/null
 apt-get install -qq -y sudo less mariadb-client &> /dev/null
 rm -rf /var/lib/apt/lists/*
+
+curl -sS -o /bin/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x /bin/wp-cli.phar
