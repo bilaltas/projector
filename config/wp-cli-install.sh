efcdf08ec944653690a899062344f05876246857
Bilal TAS <bilaltas@me.com>
2019-09-04 16:47:29 +0300

    Bug fix

--- config/wp-cli-install.sh
+++ config/wp-cli-install.sh
@@ -8,9 +8,9 @@
 # chmod +x /usr/local/bin/wp
 
 
-docker_compose exec apt-get update -qq
-docker_compose exec apt-get install -qq -y sudo less mariadb-client
-docker_compose exec rm -rf /var/lib/apt/lists/*
+docker_compose exec wpcli apt-get update -qq
+docker_compose exec wpcli apt-get install -qq -y sudo less mariadb-client
+docker_compose exec wpcli rm -rf /var/lib/apt/lists/*
 
-docker_compose exec curl -sS -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-docker_compose exec chmod +x /usr/local/bin/wp
+docker_compose exec wpcli curl -sS -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+docker_compose exec wpcli chmod +x /usr/local/bin/wp
