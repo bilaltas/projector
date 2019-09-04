5cdc83bf4a974fff4face75657415d0b905558cb
Bilal TAS <bilaltas@me.com>
2019-09-04 17:05:10 +0300

    Less outputs

--- config/wp-cli-install.sh
+++ config/wp-cli-install.sh
@@ -8,8 +8,8 @@
 # chmod +x /usr/local/bin/wp
 
 
-docker_compose exec wpcli apt-get update -qq
-docker_compose exec wpcli apt-get install -qq -y sudo less mariadb-client
+docker_compose exec wpcli apt-get update -qq &> /dev/null
+docker_compose exec wpcli apt-get install -qq -y sudo less mariadb-client &> /dev/null
 docker_compose exec wpcli rm -rf /var/lib/apt/lists/*
 
 docker_compose exec wpcli curl -sS -o /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
