8c3dc97b97bc3aed8f22a8e8bdd37272f3871a0e
Bilal TAS <bilaltas@me.com>
2019-10-11 16:02:05 +0300

    Bug fix

--- global.sh
+++ global.sh
@@ -128,7 +128,7 @@ function docker_compose {
 
 		(
 			cd "$PROJECTDIR"
-			command docker-compose -f "$BUILDERDIR/docker-compose.yml" -p "$SLUG" "$@"
+			command sudo docker-compose -f "$BUILDERDIR/docker-compose.yml" -p "$SLUG" "$@"
 		)
 	
 	else
