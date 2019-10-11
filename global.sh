819009c8c09126286cdac7c1161589cffdf765ab
Bilal TAS <bilaltas@me.com>
2019-10-11 15:14:31 +0300

    Bug fix

--- global.sh
+++ global.sh
@@ -111,8 +111,8 @@ function update_environment {
 
 
 	# Add the PROJECT_DIR and BUILDER_DIR to .env file
-	echo "PROJECT_DIR=$PROJECT_DIR" >> "$PROJECT_ENV"
-	echo "BUILDER_DIR=$BUILDER_DIR" >> "$PROJECT_ENV"
+	sudo echo "PROJECT_DIR=$PROJECT_DIR" >> "$PROJECT_ENV"
+	sudo echo "BUILDER_DIR=$BUILDER_DIR" >> "$PROJECT_ENV"
 	#echo -e "Add project and builder directories to the .env file ... ${GREEN}done${RESET}"
 
 
