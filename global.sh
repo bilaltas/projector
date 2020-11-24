e84407631d29a52e7fb597423219472d017857b3
Bilal Tas <bilaltas@me.com>
2020-11-24 17:54:06 +0300

    Bug fix

--- global.sh
+++ global.sh
@@ -408,13 +408,13 @@ function file_permission_update {
 
 function wp {
 
-	docker_compose exec -u bitnami wpcli wp "$@"
+	docker_compose exec -u root wpcli wp --allow-root "$@"
 
 }
 
 function wp_no_extra {
 
-	docker_compose exec -u bitnami wpcli wp --skip-plugins --skip-themes --skip-packages "$@"
+	docker_compose exec -u root wpcli wp --allow-root --skip-plugins --skip-themes --skip-packages "$@"
 
 }
 
